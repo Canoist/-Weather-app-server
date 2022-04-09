d012f13bb57732015a42126e2c53943e33a0ed22
Leonid Lichkun <lionproklon@gmail.com>
2022-04-09 16:26:17 +0300

    some bugs fixed

--- app.js
+++ app.js
@@ -2,12 +2,15 @@ const express = require("express");
 const mongoose = require("mongoose");
 const config = require("config");
 const chalk = require("chalk");
+const cors = require("cors");
+
 const app = express();
 const initDataBase = require("./startUp/initDataBase");
 const router = require("./routes");
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.use(cors());
 app.use("/api", router);
 
 const PORT = config.get("port") ?? 8080;
